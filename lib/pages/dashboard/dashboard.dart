6c2b99bd5d0b5b0f63ad312597ba6536eb8815ae
AyeshaSareena <ayeshasareen48@gmail.com>
2023-09-26 11:21:10 +0600

    staged changes

--- lib/pages/dashboard/dashboard.dart
+++ lib/pages/dashboard/dashboard.dart
@@ -1,8 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:aski/pages/dashboard/dashboard_search_icon.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:aski/models/user_model.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
