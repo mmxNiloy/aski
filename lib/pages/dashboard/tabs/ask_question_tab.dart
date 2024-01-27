14df80c4c965bccd099b29712337d1b9821bf430
Niloy Sarkar <dev.niloysarkar@gmail.com>
2024-01-28 01:46:59 +0600

    PDF AI assistant update: pdf uploading

--- lib/pages/dashboard/tabs/ask_question_tab.dart
+++ lib/pages/dashboard/tabs/ask_question_tab.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'dart:math';
 
 import 'package:aski/components/rich_text_editor.dart';
@@ -8,6 +9,8 @@ import 'package:aski/pages/dashboard/tabs/pdf_ai_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AskQuestionTab extends StatefulWidget {
   const AskQuestionTab({super.key});
@@ -18,64 +21,22 @@ class AskQuestionTab extends StatefulWidget {
 
 class _AskQuestionTabState extends State<AskQuestionTab> with SingleTickerProviderStateMixin {
   final TextEditingController titleController = TextEditingController();
+  List<File> _images = [];
   String _title = '';
   bool isAIQuestion = false;
   bool isPublic = false;
   final GlobalKey<FormState> _fkPost = GlobalKey<FormState>();
-  
-  // Radiam menu _animCtrRadialMenu and animations
-  late AnimationController _animCtrRadialMenu;
-  late Animation<double> scale;
-  late Animation<double> translation;
-  late Animation<double> rotation;
+
 
   @override
   void initState() {
     super.initState();
-    _animCtrRadialMenu = AnimationController(
-        vsync: this,
-        duration: const Duration(milliseconds: 500)
-    );
-
-    rotation  = Tween<double>(
-        begin: 180.0,
-        end: 360.0,
-    ).animate(
-      CurvedAnimation(
-        parent: _animCtrRadialMenu,
-        curve: const Interval(
-          0.3, 0.9,
-          curve: Curves.decelerate,
-        ),
-      ),
-    );
-
-    translation = Tween<double>(
-      begin: 0,
-      end: 100
-    ).animate(
-      CurvedAnimation(
-        parent: _animCtrRadialMenu,
-        curve: Curves.linear
-      )
-    );
-
-    scale = Tween<double>(
-        begin: 1.5,
-        end: 0
-    ).animate(
-        CurvedAnimation(
-            parent: _animCtrRadialMenu,
-            curve: Curves.fastOutSlowIn
-        )
-    );
   }
 
   @override
   void dispose() {
     super.dispose();
     titleController.dispose();
-    _animCtrRadialMenu.dispose();
   }
 
   @override
@@ -134,6 +95,21 @@ class _AskQuestionTabState extends State<AskQuestionTab> with SingleTickerProvid
                   ),
                 ),
               ),
+              Align(
+                alignment: AlignmentDirectional.centerStart,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: _images.map((imgFile) {
+                      return SizedBox(
+                          height: 128,
+                          width: 128,
+                          child: Image.file(imgFile));
+                    }).toList(),
+                  ),
+                ),
+              ),
               Row(
                 children: [
                   IconButton(
@@ -141,7 +117,9 @@ class _AskQuestionTabState extends State<AskQuestionTab> with SingleTickerProvid
                       icon: const Icon(Icons.link)
                   ),
                   IconButton(
-                      onPressed: (){},
+                      onPressed: () {
+                        handleImagePicker();
+                      },
                       icon: const Icon(Icons.image)
                   ),
                   IconButton(
@@ -173,91 +151,6 @@ class _AskQuestionTabState extends State<AskQuestionTab> with SingleTickerProvid
                   )
                 ],
               ),
-              // Radial menu
-              // AnimatedBuilder(
-              //     animation: _animCtrRadialMenu,
-              //     builder: (context, builder) => Transform.rotate(
-              //       angle: radians(rotation.value),
-              //       child: Stack(
-              //         alignment: Alignment.bottomRight,
-              //         children: [
-              //           _buildButton(
-              //               270,
-              //               translation,
-              //               child: FloatingActionButton.small(
-              //                   backgroundColor: Colors.blue,
-              //                   child: const Icon(Icons.link),
-              //                   onPressed: (){
-              //                     debugPrint('Hello from link button');
-              //                   }
-              //               )
-              //           ),
-              //           _buildButton(
-              //               225,
-              //               translation,
-              //               child: FloatingActionButton.small(
-              //                   backgroundColor: Colors.green,
-              //                   child: const Icon(Icons.image),
-              //                   onPressed: (){
-              //                     debugPrint('Hello from image button');
-              //                   }
-              //               )
-              //           ),
-              //           _buildButton(
-              //               180,
-              //               translation,
-              //               child: FloatingActionButton.small(
-              //               backgroundColor: Colors.yellow,
-              //               child: const Icon(Icons.ondemand_video),
-              //               onPressed: (){
-              //                 debugPrint('Hello from video button');
-              //               }
-              //           )
-              //           ),
-              //           _buildButton(
-              //               135,
-              //               translation,
-              //               child: FloatingActionButton.small(
-              //               backgroundColor: Colors.orange,
-              //               child: const Icon(Icons.rocket_launch),
-              //               onPressed: (){
-              //                 debugPrint('Hello from ask button');
-              //               }
-              //           )
-              //           ),
-              //           _buildButton(
-              //               90,
-              //               translation,
-              //               child: FloatingActionButton.small(
-              //             backgroundColor: Colors.red,
-              //             onPressed: (){
-              //               debugPrint('Hello from post button');
-              //             },
-              //             tooltip: 'Post',
-              //             child: const Icon(Icons.post_add),
-              //           )
-              //           ),
-              //           Transform.scale(
-              //             scale: scale.value - 1.5, // subtract the beginning value to run the opposite animation
-              //             child: FloatingActionButton.small(
-              //                 shape: const CircleBorder(),
-              //                 onPressed: _closeRadialMenu,
-              //                 backgroundColor: Colors.red,
-              //                 child: const Icon(Icons.close)
-              //             ),
-              //           ),
-              //           Transform.scale(
-              //             scale: scale.value,
-              //             child: FloatingActionButton.small(
-              //                 shape: const CircleBorder(),
-              //                 onPressed: _openRadialMenu,
-              //                 child: const Icon(Icons.more_vert)
-              //             ),
-              //           ),
-              //         ],
-              //       ),
-              //     )
-              // ),
 
             ],
           ),
@@ -315,36 +208,29 @@ class _AskQuestionTabState extends State<AskQuestionTab> with SingleTickerProvid
 
   }
 
+  Future<void> handleImagePicker() async {
+    try {
+      XFile? img = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (img == null) return;
+
+      File? imgFile = File(img.path);
+      List<File> temp = _images;
+      temp.add(imgFile);
+      setState(() {
+        _images = temp;
+      });
+    } on PlatformException catch (e) {
+      debugPrint('Signup page > Image picker > ${e.message}');
+      return;
+    }
+  }
+
   void _handleAskAI() {
     Navigator.push(
         context,
         MaterialPageRoute(
           builder: (context) => const AskAIAssistantTab()
         )
-    );
-  }
-
-  _openRadialMenu() {
-    _animCtrRadialMenu.forward();
-  }
-
-  _closeRadialMenu() {
-    _animCtrRadialMenu.reverse();
-  }
-
-  double radians(double deg) {
-    return (deg / 180.0) * pi;
-  }
-
-  Widget _buildButton(double angle, Animation<double> translation, { required Widget child }) {
-    final double rad = radians(angle);
-    return Transform(
-        transform: Matrix4.identity()..translate(
-            (translation.value) * cos(rad),
-            (translation.value) * sin(rad)
-        ),
-
-        child: child
     );
   }
 }
